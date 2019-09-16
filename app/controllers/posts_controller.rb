7d1feb9f9e106e0c3f5d874ed2bb1acbeb73a722
adamlingo <adamlingo@yahoo.com>
2019-09-16 16:40:29 -0500

    Add create action to posts, and validations for title and description in model for Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,9 +10,23 @@ class PostsController < ApplicationController
 		render json: @post
 	end
 
+	def create
+		@post = Post.new(post_params)
+
+		if @post.save
+			render json: @post, status: :created, location: @post
+		else
+			render json: @post.errors, status: :unprocessable_entity
+		end
+	end
+
 	private
 
 		def set_post
 			@post = Post.find(params[:id])
+		end
+
+		def post_params
+			params.require(:post).permit(:title, :description)
 		end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+	# validations for new Post
+	validates :title, presence: true
+	validates :description, presence: true
 end
